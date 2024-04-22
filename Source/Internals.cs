d05a5d0a47f072a175442c5eeb1ffdbe64a6717f
Adam Majcherek <Dem0n1358v2@gmail.com>
2024-04-22 04:51:38 +0200

    Update Internals.cs

--- Source/Internals.cs
+++ Source/Internals.cs
@@ -3,3 +3,4 @@
 [assembly: InternalsVisibleTo("JEM.QNet.Layers.Steamworks")]
 [assembly: InternalsVisibleTo("HoursAfterMidnight.Platforms.Desktop")]
 [assembly: InternalsVisibleTo("HolyCrusader.Platforms.Desktop")]
+[assembly: InternalsVisibleTo("TicTacBoom.Platforms.Desktop")]
